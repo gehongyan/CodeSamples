aa7ee632091f3efbde9ff15add85beb7a6466ba6
Gehongyan <gehongyan1996@126.com>
2023-10-12 21:42:27 +0800

    AvaloniaDataGridScrollBinding

---
+++ README.md
@@ -0,0 +1,5 @@
+# CodeSamples
+
+## AvaloniaDataGridScrollBinding
+
+Avalonia 两个 DataGrid 的滚动绑定，主要用于实现 DataGrid 右侧冻结列的效果。
---
+++ src/AvaloniaDataGridScrollBinding/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Sandbox.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ObservableCollection<SyncScrollDataGridItem> SyncScrollDataGridItemsSource =>
+        new(Enumerable.Range(1, 100).Select(x =>
+            new SyncScrollDataGridItem()
+            {
+                Name = $"Name {x}",
+                Value = $"Value {x}",
+            }));
+}
+
+public class SyncScrollDataGridItem
+{
+    public string? Name { get; set; }
+    public string? Value { get; set; }
+}
