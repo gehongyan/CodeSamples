b74f8135f724ba851f74a530ace8dbfc66db9cff
Gehongyan <gehongyan1996@126.com>
2023-12-08 19:24:39 +0800

    Add Span sample

---
+++ src/Span/src/Span/Program.cs
@@ -0,0 +1,9 @@
+﻿// Span<int> array = new(Enumerable.Range(1, 10000).ToArray());
+// Span<int> slice = array.Slice(2000, 1000);
+// Console.WriteLine(slice[0]);
+
+int[] array = Enumerable.Range(1, 10000).ToArray();
+int[] slice = array[2000..3000];
+Console.WriteLine(slice[0]);
+
+Span<int> asSpan = Enumerable.Range(1, 10000).ToArray().AsSpan();
